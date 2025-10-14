57ebc4ff276b6d7c20b942e26a797c6490008897
Byron Ochara <byron.ochara@gmail.com>
2025-10-14 14:19:22 +0300

    project initialised

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2remm 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0%.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>the application has started</H1>
+        <Button>Byron App</Button>
+        <Button>XXXX</Button>
+        <Input type="number" placeholder="Put correct number" />
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
