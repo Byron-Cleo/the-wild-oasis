b3755892efd2e194dfdfca4e20bfe7fa40d689b3
Byron Ochara <byron.ochara@gmail.com>
2025-10-17 20:32:14 +0300

    exported key for name chaining

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-
-const supabaseUrl = "https://ssfcarevvonsugcegupz.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzZmNhcmV2dm9uc3VnY2VndXB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0NjcxMjksImV4cCI6MjA3NjA0MzEyOX0.rIOxhrb2uWjOE6TyXsTLRlT_KyRt7Y35gMvdai1peuE";
+export const supabaseUrl = "https://ssfcarevvonsugcegupz.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzZmNhcmV2dm9uc3VnY2VndXB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0NjcxMjksImV4cCI6MjA3NjA0MzEyOX0.rIOxhrb2uWjOE6TyXsTLRlT_KyRt7Y35gMvdai1peuE";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
