9d0312237bf5abfdb00566bc2d159e9b115b8745
Byron Ochara <byron.ochara@gmail.com>
2025-10-18 13:20:10 +0300

    defined and abstracted reusable hooks for fetching data

---
+++ src/features/cabins/useCreateCabin.js
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { createEditCabin } from "../../services/apiCabins";
+
+export function useCreateCabin() {
+  const queryQlient = useQueryClient();
+
+  //CREATING CABIN DEFINITION
+  const { mutate: createCabin, isLoading: isCreating } = useMutation({
+    mutationFn: createEditCabin,
+    onSuccess: () => {
+      toast.success("New cabin successfully created");
+      queryQlient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+      //   reset();
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { isCreating, createCabin };
+}
