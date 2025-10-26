62fe88b062ee164e409fa5f5e83fd65f0fb4f39e
Byron Ochara <byron.ochara@gmail.com>
2025-10-26 12:04:10 +0300

    updated query

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,27 +1,26 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-// export async function getBookings() {
 export async function getBookings({ filter, sortBy }) {
-  console.log("---", sortBy);
 
   let query;
-  query = await supabase
+  query = supabase
     .from("bookings")
     .select(
       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
     );
 
-  if (filter !== null) {
-    query = await supabase
-      .from("bookings")
-      .select(
-        "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
-      )
-      .eq(filter.field, filter.value);
+  //FILTERING IMPLEMENTATION
+  if (filter) {
+    query = query[filter.method || "eq"](filter.field, filter.value);
   }
 
-  const { data, error } = query;
+  //SORTING IMPLEMENTATION
+  if (sortBy) {
+    query = query.order(sortBy.field);
+  }
+
+  const { data, error } = await query;
 
   if (error) {
     console.error(error);
